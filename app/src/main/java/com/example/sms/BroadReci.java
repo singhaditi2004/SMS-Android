871f0994fdcc895653482a045a5828acc9dec264
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-09-15 10:56:01 +0530

    SMS app in making

---
+++ app/src/main/java/com/example/sms/BroadReci.java
@@ -0,0 +1,25 @@
+package com.example.sms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+public class BroadReci extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle bundle=intent.getExtras();
+        Object[]smsObj= (Object[]) bundle.get("pdus");
+        for(Object obj:smsObj){
+            SmsMessage message=SmsMessage.createFromPdu((byte[])obj);
+            String mob=message.getDisplayOriginatingAddress();
+            String mess= message.getDisplayMessageBody();
+            Log.d("SMS_BODY","MOB no"+mob+"MSG"+mess);
+            SmsManager smsMana=SmsManager.getDefault();
+
+        }
+    }
+}
